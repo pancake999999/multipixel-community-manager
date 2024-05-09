be18a5d405b093714d4f3f163ea18a3499c65f1e
pancake999999 <164941367+pancake999999@users.noreply.github.com>
2024-05-09 15:45:59 -0300

    index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Watching}],
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
     status: 'dnd',
   });
 
